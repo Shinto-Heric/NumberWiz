bbb9e779b0455a6c60329cf2c592586aada8c27c
Shinto-Heric <51449554+Shinto-Heric@users.noreply.github.com>
2021-03-04 14:21:05 +0530

    First Commit

---
+++ Assets/Scenes/NumberWizard.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{ 
+
+    // Update is called once per frame
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessTest;
+    int guess;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    public void OnHigherButtonPressed()
+    {
+        min = guess;
+        guess = FindGuess(min, max);
+        guessTest.text = guess.ToString();
+    }
+
+    public void OnLowerButtonPressed()
+    {
+        max = guess;
+        guess = FindGuess(min, max);
+        guessTest.text = guess.ToString();
+    }
+
+    void StartGame()
+    {
+        guess = 0;
+        guess = FindGuess(min, max);
+        guessTest.text = guess.ToString();
+    }
+
+    int FindGuess(int min, int max)
+    {
+        guess = Random.Range(min,max + 1);
+        return guess;
+    }
+}
